4e508923446af8bec890109be2a8b41d45526a31
Show Wai Yan <showwaiyan555@gmail.com>
2026-03-26 14:18:09 +0800

    Rename again from 0 to 1

---
+++ Lab/0/Main.cpp
@@ -0,0 +1,16 @@
+
+// COS30008 - Tutorial 1, 2025
+
+#include <cstddef>
+
+extern void printIntArray( int aArray[], size_t aSize );
+
+int main()
+{
+    int lArray[] = { 23, 4, 67, 1, 0, -20, 29, -100, 32, 99, -56 };
+    size_t lSize = sizeof(lArray) / sizeof(int);
+    
+    printIntArray( lArray, lSize );
+    
+    return 0;
+}
