4343299e95d9c8e0859681241bb671d0ce6946f2
Show Wai Yan <showwaiyan555@gmail.com>
2026-04-01 23:40:43 +0800

    Finish Lab 1.2

---
+++ Lab/1.2/Polygon.cpp
@@ -0,0 +1,46 @@
+#include "Polygon.h"
+#include "Vector2D.h"
+#include <cmath>
+#include <sstream>
+#include <string>
+
+Polygon::Polygon() noexcept { fNumberOfVertices = 0; }
+
+void Polygon::readData(std::istream &aIStream) {
+  std::string line;
+  while(std::getline(aIStream,line)) {
+    std::stringstream ss(line); // inorder to read file data as stream
+    float x,y;
+    ss >> fVertices[fNumberOfVertices++];
+  }
+}
+
+size_t Polygon::getNumberOfVertices() const noexcept {
+  return this->fNumberOfVertices;
+}
+
+const Vector2D& Polygon::getVertex(size_t aIndex) const {
+  return this->fVertices[aIndex];
+}
+
+
+float Polygon::getPerimeter() const noexcept {
+  float perim = 0.0;
+  for (size_t i=0; i<fNumberOfVertices; i++) {
+    Vector2D current = fVertices[i];
+    Vector2D next = fVertices[(i+1)%fNumberOfVertices];
+    Vector2D temp = current-next;
+    perim+=std::sqrt(pow(temp.x(),2)+pow(temp.y(),2));
+  }
+  return perim;
+}
+
+Polygon Polygon::scale( float aScalar ) const noexcept {
+  Polygon result = *this;
+
+  for (size_t i=0; i<fNumberOfVertices;i++) {
+   result.fVertices[i] = this->fVertices[i]*aScalar;
+  }
+  return result;
+}
+
