1f799a7fbddd39e3f1fdc1d81f3cd47ad5c0cffc
Show Wai Yan <showwaiyan555@gmail.com>
2026-04-19 17:39:50 +0800

    Finish Problem set 1's Problem 1

---
+++ ProblemSet/1/1/PolygonPS1.cpp
@@ -0,0 +1,10 @@
+#include "Polygon.h"
+#include <cstddef>
+
+float Polygon::getSignedArea() const {
+  float result = 0.0;
+  for(size_t i = 0; i < fNumberOfVertices; i++) {
+    result += fVertices[i].cross(fVertices[(i+1)%fNumberOfVertices]);
+  }
+  return result/2;
+}
