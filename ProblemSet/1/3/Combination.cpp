3891e33e9aa5f9fa113a8844b9b2c9ab6db57431
Show Wai Yan <showwaiyan555@gmail.com>
2026-04-20 15:06:51 +0800

    Finish problem set 1's problem 3

---
+++ ProblemSet/1/3/Combination.cpp
@@ -0,0 +1,28 @@
+// COS30008, Problem Set 1/3, 2022
+
+#include "Combination.h"
+#include <cstddef>
+#include <iterator>
+
+Combination::Combination( size_t aN, size_t aK ) : fN(aN), fK(aK)
+{
+}
+
+size_t Combination::getN() const
+{
+  return fN;
+}
+
+size_t Combination::getK() const
+{
+  return fK;
+}
+
+unsigned long long Combination::operator()() const
+{
+  unsigned long long result = 1;
+  for (size_t i = 1; i <= fK; i++) {
+    result = result * (fN-(i-1)) / i;
+  }
+  return result;
+}
